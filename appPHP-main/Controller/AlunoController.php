0a20cec8fc71c862faeb30984948b5aa02eaaa0f
Cauã Luiz Fernandes <160532238+Deimos214@users.noreply.github.com>
2025-03-24 15:57:45 -0300

    alunocontroller

--- appPHP-main/Controller/AlunoController.php
+++ appPHP-main/Controller/AlunoController.php
@@ -23,4 +23,39 @@ final class AlunoController extends Controller
 
         parent::render('Aluno\lista_aluno.php',$model);
     }
+
+    public static function cadastro() : void
+    {
+        parent::isProtected();
+
+        $model = new Aluno();
+
+        try
+        {
+            if(parent::isPost())
+            {
+                $model->Id = !empty($_POST['id']) ? $_POST['id'] : null;
+                $model->Nome = $_POST['nome'];
+                $model->RA = $_POST['ra'];
+                $model->Curso = $_POST['curso'];
+                $model->save();
+
+                parent::redirect("\aluno");
+
+            } else {
+
+                if(isset($_GET['id']))
+                {
+                    $model = $model->getById( (int) $_GET['id'] );
+                }
+            }
+        } catch(Exception $e) {
+
+            $model->setError($e->getMessage());
+        }
+
+        parent::render('Aluno\form_aluno.php',$model);
+    }
+
+    public static function delete() : void
 }
